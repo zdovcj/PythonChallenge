4f4c6b776459c1dfc8203d947dc4fbd2781410a8
zdovcj <zdovcj@gmail.com>
2017-12-08 23:22:54 +0100

    Folder structure changed

--- 1/1.py
+++ 1/1.py
@@ -1,1 +1,18 @@
-print(2**38)
+def remap(s):
+    outputString = ""
+    for c in s:
+        numerical = ord(c)
+        if numerical >= 97 and numerical <= 124:
+            numerical += 2
+            if numerical > 122:
+                numerical -= 26
+            outputString += chr(numerical)
+        else:
+            outputString += c
+    return outputString
+
+
+original = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+print(remap(original))
+print()
+print("Solution:", remap("map"))
--- 2/2.py
+++ 2/2.py
@@ -1,18 +1,17 @@
-def remap(s):
-    outputString = ""
-    for c in s:
-        numerical = ord(c)
-        if numerical >= 97 and numerical <= 124:
-            numerical += 2
-            if numerical > 122:
-                numerical -= 26
-            outputString += chr(numerical)
-        else:
-            outputString += c
-    return outputString
+import collections
+from dataModule import charset
 
+counterDict = collections.OrderedDict()
 
-original = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
-print(remap(original))
-print()
-print("Solution:", remap("map"))
+for c in charset:
+    if c in counterDict:
+        counterDict[c] += 1;
+    else:
+        counterDict[c] = 1;
+
+result = ""
+for c in counterDict:
+    if counterDict[c] < 5:
+        result += c;
+
+print("Result: ", result)
